588d592b75e336697cae787c4c6713a3f820fc26
imran2908 <imranchand2908@gmail.com>
2023-10-19 20:21:37 +0530

    updated if condition for index check

--- solution/part-1/gencsv.sh
+++ solution/part-1/gencsv.sh
@@ -4,7 +4,11 @@ ei=$2
 if [[ -z $si ]] || [[ -z $ei ]]; then
   echo "Please provide the index range"
   exit
+elif (( si >= ei )); then
+  echo "The first index should be greater than last index"
+  exit
 else
+  rm -f inputFile
   for ((i=si; i<=ei; i++))
     do
       echo "$i,$((RANDOM%1000))" >> inputFile
